d3f9d5b3d0213e0eceed70bfa45360402596fa45
David <ambrosiojosedavid@gmail.com>
2025-01-24 19:11:34 -0600

    mostrar lista de amigos con HTML

--- app.js
+++ app.js
@@ -3,12 +3,24 @@ let amigos = [];
 
 function agregarAmigo(){
     let amigo = document.querySelector("#amigo").value;
-    if(amigo!=""){
+    //evalua que el campo no este vacio y que no sea un elemento reptido
+    if(amigo!=""&&!amigos.includes(amigo)){
         amigos.push(amigo);
         amigos.forEach((elemento)=>console.log(elemento));
+
+        document.querySelector("#amigo").value="";
+    //obtener la listaAmigos para actualizarla
+        var lista= document.querySelector("#listaAmigos");
+        lista.innerHTML="";
+        amigos.forEach((amigo)=>{
+            var li = document.createElement("li");
+            li.textContent= amigo;
+            lista.appendChild(li);
+
+        });
     }else{
-        alert("Por favor, ingresa un nombre");
+        alert("Por favor, ingresa un nombre válido/no repetido.");
     }
-    document.querySelector("#amigo").value="";
+    
 }
 
